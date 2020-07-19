5e6cc91f82fa226ddd1e34ed0e34ed371f0a700c
Ricardo Pinheiro <ricardo.rocha.pinheiro@gmail.com>
2020-07-20 00:10:13 +0100

    Update cache.js

--- cache.js
+++ cache.js
@@ -2,32 +2,32 @@ let memoryCache = {};
 
 // Clear cache every day
 setInterval(() => {
-	memoryCache = {};
-}, 1 * 24 * 60 * 60 * 1000);
+    memoryCache = {};
+}, 1 * 24 * 60 * 60 * 1000); // 1 day
 
 function store(key, value, ttl) {
-	memoryCache[key] = {
-		data: value,
-		expires: Date.now() + ttl * 60 * 1000
-	};
+    memoryCache[key] = {
+        data: value,
+        expires: Date.now() + ttl * 60 * 1000
+    };
 }
 
 function get(key) {
-	const cache = memoryCache[key];
+    const cache = memoryCache[key];
 
-	if (!cache) {
-		return;
-	}
+    if (!cache) {
+        return;
+    }
 
-	if (cache.expires - Date.now() < 0) {
-		delete memoryCache[key];
-		return;
-	}
+    if (cache.expires - Date.now() < 0) {
+        delete memoryCache[key];
+        return;
+    }
 
-	return cache.data;
+    return cache.data;
 }
 
 module.exports = {
-	store,
-	get
+    store,
+    get
 };
