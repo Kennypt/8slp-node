29e11a62ef3cab2219fa33a5b49499f9f3a59cc9
Ricardo Pinheiro <ricardo.rocha.pinheiro@gmail.com>
2020-07-20 00:12:17 +0100

    Update session.js

--- session.js
+++ session.js
@@ -1,37 +1,45 @@
 const client = require('./client');
 
 class Session {
-	constructor(slpSession, email, password) {
-		const { expirationDate, userId, token } = slpSession;
+    constructor(slpSession, email, password) {
+        const {
+            expirationDate,
+            userId,
+            token
+        } = slpSession;
 
-		this.expirationDate = new Date(expirationDate);
-		this.userId = userId;
-		this.token = token;
-		this.email = email;
-		this.password = password;
-	}
+        this.expirationDate = new Date(expirationDate);
+        this.userId = userId;
+        this.token = token;
+        this.email = email;
+        this.password = password;
+    }
 
-	static async create(email, password) {
-		const res = await client.login(email, password);
-		return new Session(res.session, email, password);
-	}
+    static async create(email, password) {
+        const res = await client.login(email, password);
+        return new Session(res.session, email, password);
+    }
 
-	isValid() {
-		return this.expirationDate.getTime() - Date.now() > 0;
-	}
+    isValid() {
+        return this.expirationDate.getTime() - Date.now() > 0;
+    }
 
-	async refreshToken() {
-		const res = await client.login(this.email, this.password);
-		const { expirationDate, userId, token } = res.session;
+    async refreshToken() {
+        const res = await client.login(this.email, this.password);
+        const {
+            expirationDate,
+            userId,
+            token
+        } = res.session;
 
-		if (!expirationDate || !token) {
-			throw new Error('Invalid 8SLP session data');
-		}
+        if (!expirationDate || !token) {
+            throw new Error('Invalid 8SLP session data');
+        }
 
-		this.expirationDate = new Date(expirationDate);
-		this.userId = userId;
-		this.token = token;
-	}
+        this.expirationDate = new Date(expirationDate);
+        this.userId = userId;
+        this.token = token;
+    }
 }
 
 module.exports = Session;
